b53bbb0d56cb4b4f326f7a0c4af9b036ee0e7dee
Panasonic17 <ifqyjuf17@gmail.com>
2017-03-12 18:14:29 +0200

    all ready

--- edu/src/main/java/webApp/Main.java
+++ edu/src/main/java/webApp/Main.java
@@ -10,10 +10,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("qwe");
-	Session sesion=MySesionFactory.getSessionFactory().openSession();
-	sesion.beginTransaction();
-	sesion.save(new TestHibernate("root","root"));
-	sesion.close();
+		Session sesion = MySesionFactory.getSessionFactory().openSession();
+		sesion.beginTransaction();
+		sesion.save(new TestHibernate("root", "root"));
+		sesion.close();
 	}
 
 }
