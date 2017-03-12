fe7a8a8d39ef506d3e889e18ff0eb28ffbc5b011
Panasonic17 <ifqyjuf17@gmail.com>
2017-03-12 17:38:31 +0200

    HibernateTest

---
+++ edu/src/main/java/webApp/DAO/HiberanteTestDao.java
@@ -0,0 +1,15 @@
+package webApp.DAO;
+
+import webApp.entity.TestHibernate;
+
+public class HiberanteTestDao {
+	
+	public TestHibernate getTEstHibbyID(int id){
+		return null;
+	}	
+	
+	public void addTestHibernate(TestHibernate testHibernat){
+		
+	}
+
+}
